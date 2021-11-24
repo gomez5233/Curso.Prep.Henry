ecae82c35598ac07e788e7d9701f0c4d61a2c9f9
gomez5233 <luisfercho7@hotmail.es>
2021-11-24 15:55:58 -0500

    suma

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -36,6 +36,8 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  var resultado= x+y;
+  return resultado;
 }
 
 function resta(x, y) {
