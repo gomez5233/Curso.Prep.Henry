f8bfea6fa7adb0b5632279f650fab67f42b38317
gomez5233 <luisfercho7@hotmail.es>
2021-11-26 17:55:27 -0500

    resto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -105,6 +105,8 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  var resto=x % y;
+  return resto;
 }
 
 function esPar(num) {
