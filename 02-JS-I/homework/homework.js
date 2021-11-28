6f13998164860ab9340ffdba1313232122ece011
gomez5233 <luisfercho7@hotmail.es>
2021-11-27 20:02:09 -0500

    mas funciones

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -156,16 +156,19 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
+  return Math.round(num);
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  return Math.ceil(num);
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
+  return Math.random();
 }
 
 function esPositivo(numero) {
@@ -173,6 +176,12 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+  if(numero>0)
+  return "Es positivo";
+  if (numero<0)
+  return "Es negativo";
+  if (numero==0)
+  return false;
 }
 
 function agregarSimboloExclamacion(str) {
