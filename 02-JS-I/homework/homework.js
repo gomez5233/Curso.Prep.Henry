79960bfc36bacc908797c129581565bc9101fa25
gomez5233 <luisfercho7@hotmail.es>
2021-11-27 20:55:01 -0500

    vocal

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -242,6 +242,18 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
+  if(letra.length>1)
+  return "Dato incorrecto";
+  if(letra=="a")
+  return "Es vocal";
+  if(letra=="e")
+  return "Es vocal";
+  if(letra=="i")
+  return "Es vocal";
+  if(letra=="o")
+  return "Es vocal";
+  if(letra=="u")
+  return "Es vocal";
 }
 
 
