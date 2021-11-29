d226df18d21f876a9667b9e46c89cfbd5f53a1e2
gomez5233 <luisfercho7@hotmail.es>
2021-11-29 09:02:42 -0500

    division

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -126,6 +126,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero%3==0){
+    return "fizz";
+  }
+  if(numero%5==0){
+    return "buzz";
+  }
+  else{
+    return numero;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
