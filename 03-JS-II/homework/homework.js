6b0c6898225bdde7f9cc895aafc8be64c122b271
gomez5233 <luisfercho7@hotmail.es>
2021-11-28 20:50:00 -0500

    color

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -52,7 +52,7 @@ function saludo(idioma) {
   }
   else if(idioma==="mandarin"){
     return "Ni Hao!";
-    
+
   }
   else if(idioma==="ingles"){
     return "Hello!";
@@ -70,6 +70,22 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch(color){
+    case "blue":
+      return "This is blue";
+      break;
+    case "red":
+      return "This is red";
+      break;
+    case "green":
+      return "This is green";
+      break;
+    case "orange":
+      return "This is orange";
+      break;
+    default:
+      return "Color not found";
+  }
 }
 
 function esDiezOCinco(numero) {
