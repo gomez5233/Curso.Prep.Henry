7c6d189badee623c27277a6f9505ad4a0ce850f6
gomez5233 <luisfercho7@hotmail.es>
2021-11-28 19:17:59 -0500

    dosnumeros

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,11 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  if(x>y)
-  return x;
-  if(x<y)
+  if(x>y){
+  return x;}
+  
   return y;
-  if(x===y)
-  return x || y;
+  
 }
 
 function mayoriaDeEdad(edad) {
