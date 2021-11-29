23449121d9988ad737313eba7b879e355be1edd5
gomez5233 <luisfercho7@hotmail.es>
2021-11-28 19:34:48 -0500

    estado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -28,7 +28,13 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if(status==1){
+    return "Online";
+  }else if(status==2){
+    return "Away";
+  }
 }
+
 
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
