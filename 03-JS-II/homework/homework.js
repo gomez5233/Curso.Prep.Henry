9b2ccabf6a583661e1abbebfc5598e5279d8823f
gomez5233 <luisfercho7@hotmail.es>
2021-11-29 09:49:15 -0500

    hola

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -126,13 +126,13 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if(numero%3==0)
+  if(numero%3===0)
     return "fizz";
   
-  if(numero%5==0)
+  if(numero%5===0)
     return "buzz";
 
-  if(numero%15==0 )
+  if(numero%15===0 )
   return "fizzbuzz";
 
   return numero;
