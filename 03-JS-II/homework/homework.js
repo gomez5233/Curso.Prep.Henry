f9c7c1c0e0b9f7d300d7c0643f0947dcefdf1011
gomez5233 <luisfercho7@hotmail.es>
2021-11-29 12:10:29 -0500

    uno

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -162,7 +162,7 @@ function operadoresLogicos(num1, num2, num3) {
   if(num3>num1 && num3>num2){
   return num3+1;}
 
-  if(num1===0 || num2===0 || num3===0){
+  if(num1==0 || num2==0 || num3==0){
   return "Error";}
 
   return false;
