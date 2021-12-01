b98695c1f18d6856b73c3e90505a5f0026fc6b4f
gomez5233 <luisfercho7@hotmail.es>
2021-12-01 15:51:30 -0500

    primer cambio en este archivo

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -58,7 +58,7 @@ function tienePropiedad(objeto, propiedad) {
   // Tu código:
 }
 
-function verificarPassword(usuario, password) {
+function verificarPassword(usuarios, password) {
   // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
