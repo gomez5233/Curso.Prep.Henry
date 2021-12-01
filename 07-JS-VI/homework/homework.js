90c24e0b2732ea43190de5681df7916c747530c2
gomez5233 <luisfercho7@hotmail.es>
2021-12-01 15:12:19 -0500

    prueba

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,6 +1,6 @@
 // Do not change any of the function names
 
-function mayuscula(nombre) {
+function mayuscula(Orlando) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
