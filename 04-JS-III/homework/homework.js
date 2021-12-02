26e8d585a2eb24df95890bdb25e25e0c7f080008
gomez5233 <luisfercho7@hotmail.es>
2021-12-02 18:48:04 -0500

    mayor que 18

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -136,6 +136,13 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  var cont=0;
+  for(var i=0;i<arreglo.length; i++){
+    if(arreglo[i]>18){
+      cont=cont+1;
+    }
+    return cont;
+  }
 
 }
 
