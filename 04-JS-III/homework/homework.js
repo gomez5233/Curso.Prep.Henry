0de12354a9e1e8a584b297a05184cb2a886ecf34
gomez5233 <luisfercho7@hotmail.es>
2021-12-01 19:41:41 -0500

    producto

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -120,6 +120,12 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var producto;
+  for(i=0; i<multiplicarArgumentos.length;i++){
+    producto=arguments[i];
+    return producto;
+
+  }
 }
 
 
