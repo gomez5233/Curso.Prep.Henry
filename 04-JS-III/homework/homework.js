754ad9fa93fad910f84c55f97e1a831f943b0f4f
gomez5233 <luisfercho7@hotmail.es>
2021-12-02 19:19:06 -0500

    numero

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -165,7 +165,7 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  if(n[0]==9){
+  if(n[0]===9){
     return true;
   }
   return false;
