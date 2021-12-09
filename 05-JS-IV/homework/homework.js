7d5a001cf3e8459fbdcea24953d02b2c64e618a2
gomez5233 <luisfercho7@hotmail.es>
2021-12-08 21:37:36 -0500

    objetos

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -31,9 +31,7 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
-  invocarMetodo(
-    function metodo();
-  )
+  
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
