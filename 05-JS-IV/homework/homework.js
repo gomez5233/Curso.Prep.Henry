32b50324c53e3accc4563ac3241e075432aafc71
gomez5233 <luisfercho7@hotmail.es>
2021-12-08 21:18:11 -0500

    objeto

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  const objeto = {
+    this.nombre=nombre,
+    this.edad=edad,}
+  
+  function meow{
+    return "Meow!";
+  }
+  return objeto;
 }
 
 
