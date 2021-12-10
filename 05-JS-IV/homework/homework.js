f7e5abef04a180ce37228ba6fc48afe1ab70c6af
gomez5233 <luisfercho7@hotmail.es>
2021-12-10 17:01:54 -0500

    objetos

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -56,7 +56,14 @@ function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  
+  const objeto={
+    nombre:nombre,
+    email:email,
+    password:password
+  }
 
+  return objeto;
 }
 
 function tieneEmail(usuario) {
