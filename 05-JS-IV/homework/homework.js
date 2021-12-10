1a62ad2d1258e09be02a67739a030fc364a302e4
gomez5233 <luisfercho7@hotmail.es>
2021-12-10 16:56:11 -0500

    ultimo commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -39,7 +39,7 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-  return this.numeroMisterioso*5;
+  return objetoMisterioso.numeroMisterioso*5;
 
 }
 
