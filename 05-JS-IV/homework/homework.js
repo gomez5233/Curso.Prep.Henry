0e6db7ba0eb10c4fd88f8e8165f856181baecbec
gomez5233 <luisfercho7@hotmail.es>
2021-12-12 20:58:55 -0500

    password

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -100,6 +100,8 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  this.nuevaPassword= nuevaPassword;
+  return objeto;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
