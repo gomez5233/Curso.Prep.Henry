a0dd8a9f07c1fc76689c5f4adc1b0fa72aa65a09
gomez5233 <luisfercho7@hotmail.es>
2021-12-12 22:03:36 -0500

    hoy

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -32,7 +32,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-  objeto=null;
+  objeto[property]= null;
   return objeto;
 }
 
@@ -41,6 +41,7 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]();
   
 }
 
