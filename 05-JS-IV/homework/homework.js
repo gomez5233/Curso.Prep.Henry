8eb1f1af0dbe1745031c03989500b02a12b4b4b8
gomez5233 <luisfercho7@hotmail.es>
2021-12-14 15:34:58 -0500

    porcentaje

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -173,7 +173,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
   
- 
+  producto.calcularPrecioDescuento=function(){
+    return this.precio - (this.precio*this.porcentajeDeDescuento);
+  }
+  return producto;
   }
 
   
