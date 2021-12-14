0ee4ff307c45c754b531f6c027a45e15d9d0ff69
gomez5233 <luisfercho7@hotmail.es>
2021-12-14 14:32:16 -0500

    nuevo

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -112,7 +112,7 @@ if(usuario.password===password){
 return false;
 
 }
-function actualizarPassword(usuario, nuevagPassword) {
+function actualizarPassword(usuario, nuevaPassword) {
     
     
 
@@ -120,7 +120,7 @@ function actualizarPassword(usuario, nuevagPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
-  usuario.password= nuevagPassword;
+  usuario.password= nuevaPassword;
   return usuario;
 }
 
