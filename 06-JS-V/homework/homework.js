253dd1655e2e3a8c5d03bbee1db9be1851da7137
gomez5233 <luisfercho7@hotmail.es>
2021-12-16 17:42:25 -0500

    modulo

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -26,25 +26,18 @@ function agregarStringInvertida() {
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
 
-String.prototype.reverse= function () {
-  var x = this.length;
-  var cadenaInvertida = "";
 
-  while (x>=0) {
-    cadenaInvertida = cadenaInvertida + this.charAt(x);
-    x--;
-  }
-  return cadenaInvertida;
-}
-  /*String.prototype.reverse = function() {
+
+  String.prototype.reverse = function() {
     
     var stringInvertida = '';
     for(var i = this.length - 1; i>=0; i--) {
       stringInvertida = stringInvertida + this.charAt(i);
     }
-    return stringInvertida;*/
+    return stringInvertida;
 }
 
+}
 // ---------------------------------------------------------------------------//
   //Crea el constructor de la clase "Persona"
   //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
