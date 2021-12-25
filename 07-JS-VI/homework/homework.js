339ba82da6ef7b4693ba3bfc3e6e75cc15dedd2c
gomez5233 <luisfercho7@hotmail.es>
2021-12-25 16:35:44 -0500

    reduce

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -25,6 +25,10 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var suma = numeros.reduce(function(acumulador, elemento){
+    return acumulador+elemento;
+  })
+  cb(suma)
 }
 
 function forEach(array, cb) {
